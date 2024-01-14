5e4b2c303511d6c1d2202528d7427ad61ac05f7e
AmineNaboulsi <naboulsiiamine@gmail.com>
2024-01-14 23:13:26 +0100

    upload project FAQ-accordion v3

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,5 +2,14 @@ function click_more(number){
     var icon_more = document.getElementById("icon"+number);
     icon_more.src =  path_to_default_image() ;
     console.log("click on "+"iconstar"+number);
-    
+    if(icon_more.getAttribute("data-value") == "true"){
+        console.log("get less");
+        icon_more.src = "../images/icon-minus.svg";
+        icon_more.setAttribute("data-value","false");
+    }
+    else if(icon_more.getAttribute("data-value") == "false"){
+        console.log("get more");
+        icon_more.src = "../images/icon-plus.svg";
+        icon_more.setAttribute("data-value","true");
+    }
 }
