f4ca77709ef1f5a5f74db6172fbadc3b54a6400c
AmineNaboulsi <naboulsiiamine@gmail.com>
2024-01-14 23:37:12 +0100

    Upload project FAQ v2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,17 @@
 function click_more(number){
     var icon_more = document.getElementById("icon"+number);
-    icon_more.src =  path_to_default_image() ;
+    var details = document.getElementById("q"+number);
     console.log("click on "+"iconstar"+number);
     if(icon_more.getAttribute("data-value") == "true"){
         console.log("get less");
-        icon_more.src = "../images/icon-minus.svg";
+        icon_more.src = "assets/images/icon-plus.svg";
         icon_more.setAttribute("data-value","false");
+        details.style = "display : none"
     }
     else if(icon_more.getAttribute("data-value") == "false"){
         console.log("get more");
-        icon_more.src = "../images/icon-plus.svg";
+        icon_more.src = "assets/images/icon-minus.svg";
         icon_more.setAttribute("data-value","true");
+        details.style = "display : block"
     }
 }
