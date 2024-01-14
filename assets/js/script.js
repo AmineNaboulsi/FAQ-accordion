5b9a56ff6b70d7f41c66685149122e5c9c08e858
AmineNaboulsi <naboulsiiamine@gmail.com>
2024-01-14 23:07:43 +0100

    upload project FAQ-accordion

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+function click_more(number){
+    var icon_more = document.getElementById("icon"+number);
+    icon_more.src =  path_to_default_image() ;
+    console.log("click on "+"iconstar"+number);
+    
+}
